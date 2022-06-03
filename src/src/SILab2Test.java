2c5c8a511dc0911f09b97608b521634338a69d4b
Stefan <you@example.com>
2022-06-03 03:43:46 +0200

    Dodavanje na Gradle build

---
+++ src/src/SILab2Test.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SILab2Test {
+
+    private List<String> createList(String... elems){
+        return new ArrayList<>(Arrays.asList(elems));
+    }
+
+    @Test
+    void testEveryStatement(){
+        RuntimeException ex = new RuntimeException();
+
+        ex = assertThrows(RuntimeException.class, () -> SILab2.function(createList()));
+        assertTrue(ex.getMessage().contains("List length should be greater than 0"));
+
+        ex = assertThrows(RuntimeException.class, () -> SILab2.function(createList("0","#","0")));
+        assertTrue(ex.getMessage().contains("List length should be a perfect square"));
+
+        assertEquals(createList("2","#","2","#","4","#","2","#","#"), SILab2.function(createList("0","#","0","#","0","#","0","#","#")));
+    }
+
+    @Test
+    void testEveryBranch(){
+        RuntimeException ex = new RuntimeException();
+        ex = assertThrows(RuntimeException.class, () -> SILab2.function(createList()));
+        assertTrue(ex.getMessage().contains("List length should be greater than 0"));
+
+        ex = assertThrows(RuntimeException.class, () -> SILab2.function(createList("0","#","0")));
+        assertTrue(ex.getMessage().contains("List length should be a perfect square"));
+
+        assertEquals(createList("1","0","1","#","3","#","2","#","#"), SILab2.function(createList("0","0","0","#","0","#","0","#","#")));
+
+    }
+}
